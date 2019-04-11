f287591d5bad78fd555f448bc484eef48fe732e5
Fluriko <flurikochi@gmail.com>
2019-04-12 00:10:41 +0300

    homework 8 remake 1

---
+++ src/mate/academy/hw8/NegativeParameterException.java
@@ -0,0 +1,7 @@
+package mate.academy.hw8;
+
+public class NegativeParameterException extends RuntimeException {
+      public NegativeParameterException(String message) {
+        super(message);
+    }
+}
--- src/mate/academy/hw8/PseudoRandomNumbers.java
+++ src/mate/academy/hw8/PseudoRandomNumbers.java
@@ -4,6 +4,9 @@ import java.util.stream.IntStream;
 
 public class PseudoRandomNumbers {
     public static IntStream pseudoRandomStream(int seed) {
+        if (seed < 0) {
+            throw new NegativeParameterException("Negative parameter in method");
+        }
         return IntStream.iterate(seed, n -> (n * n / 10) % 1000);
     }
 }
